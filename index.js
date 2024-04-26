681a1cd7ba4af1ec9999d899b66f6d28dd12afc9
Tarun Gupta <tarunthegreat2003@gmail.com>
2024-04-27 00:36:57 +0530

    last

---
+++ index.js
@@ -0,0 +1,16 @@
+export function tarun() {
+    const link1 = 'https://maybetarun.vercel.app/';
+    const link2 = 'https://github.com/maybetarun';
+    const link3 = 'http://www.linkedin.com/in/maybetarun';
+    const link4 = 'https://twitter.com/maybetarun';
+    const link5 = 'https://instagram.com/maybe._.tarun';
+    const links = '\n\x1b[37mMy Website:\x1b[0m \x1b[38;5;39m${link1} \n\x1b[37mGitHub:\x1b[0m \x1b[38;5;39m${link2} \n\x1b[37mLinkedIn:\x1b[0m \x1b[38;5;39m${link3} \n\x1b[37mTwitter(𝕏):\x1b[0m \x1b[38;5;39m${link4} \n\x1b[37mInstagram:\x1b[0m \x1b[38;5;39m${link5}\x1b[37m';
+    console.log('\n');
+    console.log('Heyy!! I am \x1b[38;5;39mTarun Gupta\x1b[37m, also known by the alias \x1b[38;5;137mbread. \x1b[38;5;8myou ask why? even idk ;-;');
+    console.log('\x1b[37mI am an engineer. \x1b[38;5;8mI can do anything but engineering.');
+    console.log('\x1b[37mI like to code \x1b[32mr\x1b[34ma\x1b[35mn\x1b[36md\x1b[31mo\x1b[38;5;200mm \x1b[37mprograms, build \x1b[36mcool \x1b[37mlooking websites, \x1b[35mdesign \x1b[37mstuff and a lot of other \x1b[32mr\x1b[34ma\x1b[35mn\x1b[36md\x1b[31mo\x1b[38;5;200mm \x1b[37mahh things.');
+    console.log('Always looking forward to connect with people, learn new stuff and just grow.');
+    console.log(links);
+}
+
+tarun();
